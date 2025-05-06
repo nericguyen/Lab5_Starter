d232bbb1d0b9f89670f960b7abbcf802c7458a05
Eric Nguyen <nericguyen@gmail.com>
2025-05-05 20:53:23 -0700

    Part 2: Explore

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Lab 5 - Starter
 
 Eric Nguyen
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+You would NOT use a unit test for a message feature since it would probably require a lot of interactions between many parts, which
+is not what unit testing is for.  If the message feature was not working properly, it would be difficult to pinpoint where exactly
+the fault would come from.
+
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Yes, you would want to use a unit test to test the max length feature since this feature is probably encapsulated in a small amount of code
+and can be easily bugtested.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,70 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  /**
+   * On page load, all of the available voices that you can use for your SpeechSynthesizer 
+   * should be loaded and populate the “Select Voice” dropdown. 
+   * (These are browser specific, so you might get different ones browser to browser).
+   */
+  let voiceSelect = document.getElementById("voice-select");
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = speechSynthesis.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  /**
+   * When you click the “Press to Talk” button, the following should happen:
+   * The text that you have typed into the “Text to speak here” text area should be spoken out loud 
+   * using the voice that you have selected
+   */
+  let speakButton = document.querySelector("button");
+  let speakImg = document.getElementById("explore").querySelector("img");
+  
+  //Updates the text to speak as it is typed
+  let speakText = document.getElementById("text-to-speak");
+  speakText.addEventListener("input", (event) => {
+    speakText.textContent = event.target.value;
+  });
+
+  speakButton.addEventListener("click", speakFunc);
+  function speakFunc() {
+    //Create utterance and selects voice
+    let selectedOption = voiceSelect.options[voiceSelect.selectedIndex].value;
+    let wordsToSpeak = new SpeechSynthesisUtterance(speakText.textContent);
+    for (let i = 0; i < voices.length; i++) {
+      if (`${voices[i].name} (${voices[i].lang})` === selectedOption) {
+        wordsToSpeak.voice = voices[i];
+        break;
+      }
+    }
+
+    //Speaking + Animations
+    speakImg.src = `assets/images/smiling-open.png`;
+    speechSynthesis.speak(wordsToSpeak);
+    wordsToSpeak.addEventListener("end", () => {
+      speakImg.src = `assets/images/smiling.png`;
+    });
+  }
+
+  
 }
